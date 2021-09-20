b0b370c7d30e8e0de83efb624bf33e93a2498355
halimyusuf <haleemyoosuph@gmail.com>
2021-09-20 13:48:18 +0100

    chore: style footer anchor tag

--- pages/footer.js
+++ pages/footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 export default function Footer() {
   return (
     <>
@@ -8,29 +9,57 @@ export default function Footer() {
           <div style={{ display: "flex", flexWrap: "wrap" }}>
             <div className="footer-col">
               <div className="footer-sub-head">Features</div>
-              <div className="footer-item">Link Shortening</div>
-              <div className="footer-item"> Branded Links</div>
-              <div className="footer-item">Analytics</div>
+              <div className="footer-item">
+                <a href="#">Link Shortening </a>
+              </div>
+              <div className="footer-item">
+                <a href="#"> Branded Links </a>
+              </div>
+              <div className="footer-item">
+                <a href="#">Analytics </a>
+              </div>
             </div>
             <div className="footer-col">
               <div className="footer-sub-head">Resources</div>
-              <div className="footer-item">Blog</div>
-              <div className="footer-item">Developers</div>
-              <div className="footer-item">Support</div>
+              <div className="footer-item">
+                <a href="#">Blog </a>
+              </div>
+              <div className="footer-item">
+                <a href="#">Developers </a>
+              </div>
+              <div className="footer-item">
+                <a href="#">Support </a>
+              </div>
             </div>
             <div className="footer-col">
               <div className="footer-sub-head">Company</div>
-              <div className="footer-item">About</div>
-              <div className="footer-item">Our Team</div>
-              <div className="footer-item">Careers</div>
-              <div className="footer-item">Contact</div>
+              <div className="footer-item">
+                <a href="#">About </a>
+              </div>
+              <div className="footer-item">
+                <a href="#">Our Team </a>
+              </div>
+              <div className="footer-item">
+                <a href="#">Careers </a>
+              </div>
+              <div className="footer-item">
+                <a href="#">Contact </a>
+              </div>
             </div>
 
             <div className="footer-col footer-icon">
-              <img src="/images/icon-facebook.svg" />
-              <img src="/images/icon-twitter.svg" />
-              <img src="/images/icon-pinterest.svg" />
-              <img src="/images/icon-instagram.svg" />
+              <a href="#">
+                <img src="/images/icon-facebook.svg" />
+              </a>
+              <a href="#">
+                <img src="/images/icon-twitter.svg" />
+              </a>
+              <a href="#">
+                <img src="/images/icon-pinterest.svg" />
+              </a>
+              <a href="#">
+                <img src="/images/icon-instagram.svg" />
+              </a>
             </div>
           </div>
         </div>
