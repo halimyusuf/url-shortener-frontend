82ecdb7aba5e64b8b3f829fa90a33a6a205eae2b
halimyusuf <haleemyoosuph@gmail.com>
2021-09-20 14:13:35 +0100

    chore: fix anchor tag error

--- pages/footer.js
+++ pages/footer.js
@@ -67,7 +67,11 @@ export default function Footer() {
 
       <div className="attribution">
         Challenge by
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
           Frontend Mentor
         </a>
         . Coded by <a href="https://github.com/halimyusuf">Halim Yusuf</a>.
