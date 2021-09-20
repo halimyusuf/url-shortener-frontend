0ce6e296b3a783d2d922f051885b0dd7f8a8d0e7
halimyusuf <haleemyoosuph@gmail.com>
2021-09-20 14:11:26 +0100

    chore: add my github url

--- pages/footer.js
+++ pages/footer.js
@@ -70,7 +70,7 @@ export default function Footer() {
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Yusuf Halim</a>.
+        . Coded by <a href="https://github.com/halimyusuf">Halim Yusuf</a>.
       </div>
     </>
   );
