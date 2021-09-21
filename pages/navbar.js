8739afc93c5ea9399d80786b123ce38d2038aa98
halimyusuf <haleemyoosuph@gmail.com>
2021-09-21 13:34:57 +0100

    chore: add nav toggle

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,39 +1,64 @@
+import { useState } from "react";
+
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  function onToggle() {
+    setOpen(!open);
+  }
+
   return (
     <nav className="navbar">
-      <ul>
-        <li id="nav-brand">
-          <a>Shorly</a>
-        </li>
-        <li>
-          <a href="#" className="nav-btn">
-            Features
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-btn">
-            Pricing
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-btn">
-            Resources
-          </a>
-        </li>
-      </ul>
+      <div className="px-4 pt-2" id="nav-brand">
+        <a>Shorly</a>
+      </div>
+      <div className={`nav-links ${open ? "" : "nav-close"}`}>
+        <div onClick={onToggle} className="nav-toggle-btn">
+          <div className={`line ${open ? "nav-toggled" : ""}`}></div>
+          <div className={`line ${open ? "nav-toggled" : ""}`}></div>
+          <div className={`line ${open ? "nav-toggled" : ""}`}></div>
+        </div>
+        <div className="d-flex">
+          <div>
+            <a href="#" className="nav-btn">
+              Features
+            </a>
+          </div>
+          <div>
+            <a href="#" className="nav-btn">
+              Pricing
+            </a>
+          </div>
+          <div>
+            <a href="#" className="nav-btn">
+              Resources
+            </a>
+          </div>
+        </div>
 
-      <ul>
-        <li>
-          <a href="#" className="nav-btn active">
-            Login
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-btn">
-            Sign Up
-          </a>
-        </li>
-      </ul>
+        {/* <div style={{ flex: 1, width: "100%" }}> </div> */}
+
+        <div className="d-flex">
+          <div>
+            <a href="#" className="nav-btn active">
+              Login
+            </a>
+          </div>
+          <div>
+            <a href="#" className="nav-btn">
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </div>
+      <div onClick={onToggle} className="nav-toggle-btn pt-2">
+        <div className={`line ${open ? "nav-toggled" : ""}`}></div>
+        <div className={`line ${open ? "nav-toggled" : ""}`}></div>
+        <div className={`line ${open ? "nav-toggled" : ""}`}></div>
+      </div>
+      {/* <div className="nav-toggle-open">
+        <div> </div>
+      </div> */}
     </nav>
   );
 }
